ad9377de264be7c25745e3ef6637404ec33bdffc
gulshancod <gulshan.kr2008@gmail.com>
2026-09-20 00:29:24 +0530

    Fix API CORS authorization header

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.header(
     "Access-Control-Allow-Methods",
